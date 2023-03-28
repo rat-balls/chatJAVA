07273b68dfa4a942e93271fe230054fcb24486a3
E.Delalande <delalande.ethan@gmail.com>
2023-03-28 11:32:11 +0200

    it work

--- src/Client.java
+++ src/Client.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
+import java.net.Inet4Address;
 import java.net.Socket;
 
 public class Client implements Runnable{
@@ -13,7 +13,7 @@ public class Client implements Runnable{
     @Override
     public void run() {
         try{
-            client = new Socket(InetAddress.getLocalHost(), 9070);
+            client = new Socket("10.57.28.210", 9070);
             out = new PrintWriter(client.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
             InputHandler inputHandler = new InputHandler();
